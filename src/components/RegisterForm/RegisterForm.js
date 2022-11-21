9bc98e0d75c2bf8bcc47a643449277576618a175
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-11-21 02:12:50 +0200

    Update RegisterForm.js

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -11,7 +11,6 @@ export const RegisterForm = () => {
   const handleSubmit = e => {
     
     e.preventDefault();
-    console.log(e)
     const form = e.currentTarget;
     dispatch(
       register({
